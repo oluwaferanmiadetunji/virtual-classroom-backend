8cc2d6944409d7a98c94ebe0a86347fef38a3ded
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-02-25 22:16:58 +0100

    First commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request: any, response: { send: (arg0: string) => void }) => {
+	functions.logger.info('Hello logs!', { structuredData: true });
+	response.send('Hello from Firebase!');
+});
